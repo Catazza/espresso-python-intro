27effcdaf5d32f5183a98dca35296b59ded6bacb
Manimegalai Jaganathan <manimegalai.jaganathan@sapient.com>
2019-02-12 10:11:27 +0000

    adding the setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='espresso-python',
+    packages=find_packages(),
+    version='0.1.0',
+    description='training package creation',
+    author='Manimegalai Jaganathan',
+    license='',
+)
